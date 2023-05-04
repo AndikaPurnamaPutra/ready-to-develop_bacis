d5862678c61f8d856ce264dea8f8d7acfcc1f7c6
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-05-04 16:21:13 +0700

    revisi-02 dari progress 01

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ navbarToggle.onclick = () => {
   navBar = document.querySelector(".container-navbar");
   navBar.classList.toggle("active");
 };
+
+const navbar = document.querySelector("header");
+const navbarOffset = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.scrollY >= navbarOffset) {
+    navbar.classList.add("sticky");
+  } else if (window.scrollY <= navbarOffset) {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.addEventListener("scroll", stickyNav);
