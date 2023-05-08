770c89c425556a59fbe6262e7c08a9e6cb9e5268
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-05-08 16:57:13 +0700

    progress03-(section01,section09)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar navbarToggle
 navbarToggle = document.querySelector(".navbar-toggle");
 navbarToggle.onclick = () => {
   navBar = document.querySelector(".container-navbar");
@@ -7,6 +8,7 @@ navbarToggle.onclick = () => {
 const navbar = document.querySelector("header");
 const navbarOffset = navbar.offsetTop;
 
+// StickyNav Navbar
 function stickyNav() {
   if (window.scrollY >= navbarOffset) {
     navbar.classList.add("sticky");
@@ -16,3 +18,14 @@ function stickyNav() {
 }
 
 window.addEventListener("scroll", stickyNav);
+
+// Slider
+let counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 5000);
